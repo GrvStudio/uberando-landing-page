872cad9d5c4f63e355680cdcbf84464dfc5eb42c
ceaton9 <ceaton9@asu.edu>
2024-07-06 22:15:20 +0700

    chore(.gitignore): add .netlify folder to .gitignore to exclude local Netlify files feat(nav.tsx): add toggleMenu function to Navbar component for mobile menu functionality

--- src/components/organisms/navbar/nav.tsx
+++ src/components/organisms/navbar/nav.tsx
@@ -1,11 +1,25 @@
+import React, { useState } from 'react';
 import Logo from "components/atoms/logos/berando";
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav className="bg-gray-900">
+        <nav className="bg-gray-900 sm:mb-44">
             <div className="container mx-auto pt-6 px-4 py-2 flex flex-col md:flex-row justify-between items-center">
                 <Logo />
-                <div className="space-x-2 md:space-x-4 bg-[#2C2C2C] px-6 md:px-14 py-4 md:py-8 rounded-full bg-opacity-80 mb-4 md:mb-0">
+                <div className="md:hidden">
+                    <button onClick={toggleMenu} className="text-white focus:outline-none">
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}></path>
+                        </svg>
+                    </button>
+                </div>
+                <div className={`flex-col md:flex-row md:flex ${isOpen ? 'flex' : 'hidden'} md:items-center space-y-4 md:space-y-0 md:space-x-4 bg-[#2C2C2C] px-6 md:px-14 py-4 md:py-8 sm:w-full  sm:rounded-full bg-opacity-80 mb-4 md:mb-0`}>
                     <a href="#about" className="hanken-grotesk-legendary-400 bg-gradient-to-r inline from-[#2670F6] to-[#00F0C5] bg-clip-text text-transparent">About Us</a>
                     <a href="#services" className="text-white hanken-grotesk-legendary-400 hover:text-gray-400">Services</a>
                     <a href="#scope" className="text-white hanken-grotesk-legendary-400 hover:text-gray-400">Our Scope</a>
@@ -15,5 +29,5 @@ export default function Navbar() {
                 <button className="bg-gradient-to-r from-[#2670F6] hanken-grotesk-legendary-500 to-[#00F0C5] text-white px-6 md:px-12 py-2 md:py-4 rounded-xl">Get Started</button>
             </div>
         </nav>
-    )
+    );
 }
