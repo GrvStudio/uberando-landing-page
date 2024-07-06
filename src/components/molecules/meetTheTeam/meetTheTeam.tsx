63580b0a66a751e534033da2cedec9071fa74d90
ceaton9 <ceaton9@asu.edu>
2024-07-06 21:05:53 +0700

    feat(meetTheTeam.tsx): update team member name to remove unnecessary prefix style(meetTheTeam.tsx): improve text styling for team member names and positions style(style.css): add CSS class for truncating multiline text in team member names and positions

--- src/components/molecules/meetTheTeam/meetTheTeam.tsx
+++ src/components/molecules/meetTheTeam/meetTheTeam.tsx
@@ -12,7 +12,7 @@ interface TeamMember {
 }
 
 const teamMembers: TeamMember[] = [
-    { id: 1, name: 'NOR CURTIS-LEWIS', position: 'Head of Finance', img: 'https://i.pravatar.cc/300' },
+    { id: 1, name: 'CURTIS-LEWIS', position: 'Head of Finance', img: 'https://i.pravatar.cc/300' },
     { id: 2, name: 'AMANDA', position: 'Business Analyst', img: 'https://i.pravatar.cc/300' },
     { id: 3, name: 'ALEX XIAO', position: 'Lead Software Engineer', img: 'https://i.pravatar.cc/300' },
     { id: 4, name: 'BRADLEY', position: 'Chief Operations Officer', img: 'https://i.pravatar.cc/300' },
@@ -48,7 +48,7 @@ const MeetTheTeam: React.FC = () => {
 
     return (
         <div className="bg-gray-900 text-white py-12 px-6 md:px-12">
-            <h2 className="text-[67px] text-center sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4 hanken-grotesk-legendary-400">
+            <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4 hanken-grotesk-legendary-400 text-center">
                 Meet The Team
             </h2>
             <Slider {...settings}>
@@ -58,8 +58,8 @@ const MeetTheTeam: React.FC = () => {
                             <div className="mb-4">
                                 <img className="rounded-full w-32 h-32 md:w-48 md:h-48 lg:w-56 lg:h-56" src={member.img} alt={member.name} />
                             </div>
-                            <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2">{member.name}</h3>
-                            <p className="hanken-grotesk-legendary-400 bg-gradient-to-r inline from-[#2670F6] to-[#00F0C5] bg-clip-text text-transparent">{member.position}</p>
+                            <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2 truncate-multiline">{member.name}</h3>
+                            <p className="hanken-grotesk-legendary-400 bg-gradient-to-r inline from-[#2670F6] to-[#00F0C5] bg-clip-text text-transparent text-sm md:text-base lg:text-lg">{member.position}</p>
                         </div>
                     </div>
                 ))}
