920438ed533666ee41923e69e33d518d8075523a
ceaton9 <ceaton9@asu.edu>
2024-07-06 15:04:05 +0700

    docs(index.html): update title of the webpage to "Uberando - Igniting Ideas Inspiring Innovation." style(emailForm.tsx): adjust column span and width of input field to improve layout and alignment

--- src/components/atoms/emailForm/emailForm.tsx
+++ src/components/atoms/emailForm/emailForm.tsx
@@ -6,14 +6,14 @@ const EmailForm = () => {
             <div className="container mx-auto px-4 text-center">
                 <Box className="mb-12" />
                 <div className="grid grid-cols-1 md:grid-cols-5 items-center gap-4">
-                    <p className="text-lg md:text-xl mb-8 col-span-1 md:col-span-2 hanken-grotesk-legendary-500">
+                    <p className="text-lg md:text-xl mb-8 col-span-2 md:col-span-2 hanken-grotesk-legendary-500">
                         We specialize in custom software development, delivering tailored products, IT services, and comprehensive end-to-end solutions for our enterprise clients.
                     </p>
                     <div className="flex flex-col md:flex-row justify-center md:justify-end h-14 col-span-1 md:col-span-3">
                         <input
                             type="email"
                             placeholder="Enter Your Email"
-                            className="p-2 rounded-lg mb-4 md:mb-0 md:mr-6 border bg-[#2C2C2C] bg-opacity-80 w-full md:w-[27%]"
+                            className="p-2 rounded-lg mb-4 md:mb-0 md:mr-6 border bg-[#2C2C2C] bg-opacity-80 w-full md:w-[57%]"
                         />
                         <button className="bg-gradient-to-r w-full md:w-[20%] from-[#2670F6] to-[#00F0C5] text-white p-2 rounded-lg hover:bg-blue-600">Get Started</button>
                     </div>
