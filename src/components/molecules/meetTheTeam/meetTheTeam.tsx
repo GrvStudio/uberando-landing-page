af7406b71031fc2504412ec217b63945a9d1d8cf
ceaton9 <ceaton9@asu.edu>
2024-07-06 21:47:18 +0700

    style(meetTheTeam.tsx): add missing semicolon to import statement for consistency and code readability

--- src/components/molecules/meetTheTeam/meetTheTeam.tsx
+++ src/components/molecules/meetTheTeam/meetTheTeam.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./style.css"
+import "./style.css";
 
 interface TeamMember {
     id: number;
