bd3f117a5b2de80d69857df5032d2055faac1665
ceaton9 <ceaton9@asu.edu>
2024-07-06 15:11:03 +0700

    feat: updating companyCard

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,4 @@
+import CompanyCard from "components/atoms/card/companyCard";
 import EmailForm from "components/atoms/emailForm/emailForm";
 import HeroSection from "components/atoms/hero/heroSection";
 import Navbar from "components/organisms/navbar/nav";
@@ -6,9 +7,13 @@ function App() {
   return (
     <main>
       <div className="h-screen w-screen">
+        {/* Section One */}
         <Navbar />
         <HeroSection />
         <EmailForm />
+
+        {/* Section Two */}
+        <CompanyCard />
       </div>
     </main>
   );
---
+++ src/components/atoms/card/companyCard.tsx
@@ -0,0 +1,43 @@
+import FrameOne from "../../../assets/Frame_one.svg";
+import FrameTwo from "../../../assets/Frame_two.svg";
+import FrameThree from "../../../assets/Frame_three.svg";
+
+const companyList = [
+    {
+        id: 1,
+        image: FrameTwo,
+    },
+    {
+        id: 2,
+        image: FrameThree,
+    },
+    {
+        id: 3,
+        image: FrameOne,
+    },
+    {
+        id: 4,
+        image: FrameTwo,
+    },
+    {
+        id: 5,
+        image: FrameThree,
+    },
+]
+
+export default function CompanyCard() {
+    return (
+        <section className="bg-gray-900 text-white py-6">
+            <div className="container mx-auto flex flex-wrap items-center justify-between">
+                {
+                    companyList.map((company) => (
+                        <div key={company.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 py-2 text-center">
+                            <company.image />
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+
+    )
+}
